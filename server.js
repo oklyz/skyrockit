905191cc217d3a9a73127a978148a82e934844c5
Ali Hasan Habib <alimodffs2@gmail.com>
2025-07-15 18:56:53 +0300

    add style

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const mongoose = require("./config/db")
 // Set the port configurations
 const port = process.env.PORT ? process.env.PORT : "3000"
 
+const path = require("path")
 // Require Middlewares
 const methodOverride = require("method-override")
 const morgan = require("morgan")
@@ -20,6 +21,8 @@ const isSignin = require("./middleware/is-sign-in")
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use(morgan("dev"))
+
+app.use(express.static(path.join(__dirname, "public")))
 
 // Session Configurations
 app.use(
